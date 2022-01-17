ea3e18d28a0e0af19b0c1813abeed1fa170f9143
Yann Gouffon <github@yago-art.ch>
2022-01-17 11:04:18 +0100

    ✨ add header

---
+++ src/components/HeaderMobile.tsx
@@ -0,0 +1,88 @@
+import { createSignal } from 'solid-js';
+
+const HeaderMobile = () => {
+  const [open, setOpen] = createSignal(false);
+
+  return (
+    <nav className="block bg-white shadow sm:hidden" aria-hidden="true">
+      <div className="px-2 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="relative flex justify-between h-16">
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            <button
+              type="button"
+              className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+              onClick={() => setOpen(!open())}
+            >
+              <span className="sr-only">Open main menu</span>
+              {!open() && (
+                <svg
+                  className="block w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+              {open() && (
+                <svg
+                  className="w-6 h-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+          <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-between">
+            <div className="flex items-center flex-shrink-0">
+              <img
+                className="block w-auto h-8 lg:hidden"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt="Workflow"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={open() ? 'sm:hidden' : 'hidden'} id="mobile-menu">
+        <div className="pt-2 pb-4 space-y-1">
+          {['Dashboard', 'Team', 'Projects', 'Calendar'].map(item => (
+            <a
+              href="#something"
+              key={`item-${item}-mobile`}
+              className={
+                item === 'Dashboard'
+                  ? 'bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                  : 'border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+              }
+            >
+              {item}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderMobile;
